4df6f23e01af7f0fea61c942100d3c1eb251f7e1
LO Kam Tao Leo <leolo@leolo.org>
2018-03-30 13:24:35 +0800

    Added token check for ConfigReload

--- src/main/java/org/leolo/album/ConfigReload.java
+++ src/main/java/org/leolo/album/ConfigReload.java
@@ -1,6 +1,8 @@
 package org.leolo.album;
 
 import java.io.IOException;
+import java.util.Arrays;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +15,7 @@ import org.slf4j.LoggerFactory;
 /**
  * Servlet implementation class ConfigReload
  */
-@WebServlet("/ConfigReload")
+@WebServlet("/ConfigReload/*")
 public class ConfigReload extends HttpServlet {
 	
 	Logger logger = LoggerFactory.getLogger(ConfigReload.class);
@@ -33,15 +35,34 @@ public class ConfigReload extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String rId = Utils.getNextRequestId();
+		String [] tokens = Utils.getURLPart(request);
+		logger.info("tokens={}", Arrays.toString(tokens));
 		JSONResponse resp = new JSONResponse();
-		logger.warn("Request {} initized config reload.", rId);
-		resp.put("sdatetime", Utils.getISO8601Time());
-		ConfigManager.getInstance().reload(request.getServletContext().getResource("/WEB-INF/config"));
-		resp.put("edatetime", Utils.getISO8601Time());
-		resp.put("Result","OK");
-		resp.put("requestId", rId);
-		response.setContentType(resp.getContentType());
-		resp.write(response.getOutputStream());
+		if(tokens.length<=1){
+			resp.put("Result","Error");
+			resp.put("info", "Token required");
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			resp.put("requestId", rId);
+			response.setContentType(resp.getContentType());
+			resp.write(response.getOutputStream());
+		}else if(tokens[1].equals(ConfigManager.getInstance().getString("system.config.reloadToken"))){
+			logger.warn("Request {} initized config reload.", rId);
+			resp.put("sdatetime", Utils.getISO8601TimeWithMilliSecond());
+			ConfigManager.getInstance().reload(request.getServletContext().getResource("/WEB-INF/config"));
+			resp.put("edatetime", Utils.getISO8601TimeWithMilliSecond());
+			resp.put("Result","OK");
+			resp.put("requestId", rId);
+			response.setContentType(resp.getContentType());
+			resp.write(response.getOutputStream());
+		}else{
+			logger.warn("Request {} initized config reload, but without valid token", rId);
+			resp.put("Result","Error");
+			resp.put("info", "Invalid token");
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			resp.put("requestId", rId);
+			response.setContentType(resp.getContentType());
+			resp.write(response.getOutputStream());
+		}
 	}
 
 	/**
--- src/main/java/org/leolo/album/Utils.java
+++ src/main/java/org/leolo/album/Utils.java
@@ -8,7 +8,10 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Random;
+import java.util.StringTokenizer;
 import java.util.TimeZone;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.codec.binary.Base64;
 import org.mindrot.jbcrypt.BCrypt;
@@ -16,7 +19,9 @@ import org.mindrot.jbcrypt.BCrypt;
 public class Utils {
 //	private static Calendar calendar = Calendar.getInstance();
 	private static SimpleDateFormat dateFormatISO8601 = new SimpleDateFormat(
-        "yyyy-MM-dd'T'HH:mm:ssZ", Locale.US);
+	        "yyyy-MM-dd'T'HH:mm:ssZ", Locale.US);
+	private static SimpleDateFormat dateFormatISO8601ms = new SimpleDateFormat(
+	        "yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.US);
 	static{
 		dateFormatISO8601.setTimeZone(TimeZone.getTimeZone("GMT"));
 	}
@@ -35,6 +40,16 @@ public class Utils {
 		return dateFormatISO8601.format(calendar.getTime());
 	}
 	
+	
+	public static String getISO8601TimeWithMilliSecond(){
+		return getISO8601TimeWithMilliSecond(new Date());
+	}
+	
+	public static String getISO8601TimeWithMilliSecond(Date time){
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(time);
+		return dateFormatISO8601ms.format(calendar.getTime());
+	}
 	public static String getNextRequestId(){
 		byte [] randomId = new byte[12];
 		requestIdRandomProvider.nextBytes(randomId);
@@ -54,7 +69,21 @@ public class Utils {
 		map.put("version", "0.0.1");
 		map.put("min-api-level", "0");
 		map.put("max-api-level", "0");
-		map.put("extension", new ArrayList<String>());
+		ArrayList<String> ext = new ArrayList<String>();
+		ext.add("album.db");
+		map.put("extension", ext);
 		return map;
+	}
+	
+	public static String [] getURLPart(HttpServletRequest request){
+        String requestURL = request.getRequestURI().substring(request.getContextPath().length());
+        ArrayList<String> tokensList = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(requestURL,"/");
+        while(st.hasMoreTokens()){
+            tokensList.add(st.nextToken());
+        }
+        String [] tokenArray = new String[tokensList.size()];
+        tokensList.toArray(tokenArray);
+        return tokenArray;
 	}
 }
